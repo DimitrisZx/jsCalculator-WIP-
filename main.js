ac0154a723db59d4fa7b62a4eace391d9d29d9cc
Dimitris <dimitriszarahanis@gmail.com>
2019-01-01 13:19:03 +0200

    Second commit

--- main.js
+++ main.js
@@ -1,52 +1,42 @@
-
-// class Operation {
-//   constructor(type) {
-//     this.type;
-//   }
-
-//   static addition() {
-//     const num1 = parseInt(display.value);
-//     console.log(num1)
-//     display.value = 0;
-//     memory.numVal = num1
-//     console.log(memory);
-
-//   }
-//   static subtraction() {
-
-//   }
-//   static multiplication() {
-
-//   }
-//   static division() {
-
-//   }
-//   static clear() {
-
-//   }
-// }
-
 class Memory {
   constructor() {
     this.numbers = [];
     this.operationType = '';
+    this.calcArr = [];
+    this.result = 0;
   }
 }
 
-const memory = new Memory;
+let memory = new Memory;
 
 // Events..
 const display = document.querySelector('#input');
 display.value = 0;
-const add = document.querySelector("#add").addEventListener("click", (e) => { e.preventDefault; memory.operationType = 'addition'; console.log(memory) });
+const add = document.querySelector("#add").addEventListener("click", (e) => {
+  e.preventDefault;
+  memory.operationType = 'addition';
+  memory.calcArr.push(memory.numbers[0]);
+  memory.numbers = [];
+  display.value = '0';
+
+});
 const subtract = document.querySelector("#sub").addEventListener("click", (e) => { e.preventDefault; memory.operationType = 'subtraction' });
 const multiply = document.querySelector("#mult").addEventListener("click", (e) => { e.preventDefault; memory.operationType = 'multiplication' });
 const divide = document.querySelector("#divide").addEventListener("click", (e) => { e.preventDefault; memory.operationType = 'division' });
-const equal = document.querySelector("#equals").addEventListener("click", (e) => { e.preventDefault; getResult() });
-const point = document.querySelector('#point').addEventListener('click', () => { if (display.value !== '0') display.value += '.' })
+const equal = document.querySelector("#equals").addEventListener("click", (e) => {
+  e.preventDefault;
+  getResult()
+});
+const point = document.querySelector('#point').addEventListener('click', (e) => {
+  e.preventDefault;
+  if (display.value !== '0');
+  display.value += '.';
+  memory.numbers.push('.');
+  memory.result += memory.calcArr.reduce((acc, curr) => acc + curr);
+})
 
-const clear = document.querySelector("#clear").addEventListener("click", (e) => { display.value = 0; memory.numbers = ''; memory.operationType = ''; }); // Clear display and Memory
-const logMem = document.querySelector("#logMem").addEventListener('click', (e) => { e.preventDefault(); console.log(`Numbers: ${memory.numbers} | OpType: ${memory.operationType}`) })
+const clear = document.querySelector("#clear").addEventListener("click", (e) => { display.value = 0; memory.numbers = []; memory.operationType = ''; memory.calcArr = []; }); // Clear display and Memory
+const logMem = document.querySelector("#logMem").addEventListener('click', (e) => { e.preventDefault(); console.log(`Numbers: ${memory.numbers} CalcArr: ${memory.calcArr}| OpType: ${memory.operationType}`) })
 
 const numInputBtns = document.querySelectorAll('.btn-num').forEach(btn => {
   btn.addEventListener('click', (e) => {
@@ -54,16 +44,27 @@ const numInputBtns = document.querySelectorAll('.btn-num').forEach(btn => {
     if (display.value === '0') display.value = '';
     display.value += btn.textContent;
     memory.numbers.push(btn.textContent);
-    memory.numbers.reduce((acc, curr) => acc + curr)
-    console.log(memory);
+    const number = memory.numbers.join('');
+    memory.numbers = [];
+    memory.numbers.push(number)
+
+
+
   });
 });
 
 function getResult() {
   switch (memory.operationType) {
     case 'addition':
-      const result = parseFloat(memory.numVal1) + parseFloat(memory.numVal2);
+      memory.calcArr.push(memory.numbers);
+      let result = memory.calcArr.reduce((acc, curr) => parseFloat(acc) + parseFloat(curr));
+      memory.numbers = [];
+      console.log(memory.calcArr)
+      memory.numbers.push(result)
+      memory.calcArr = [];
       display.value = result;
+      console.log(`Numbers: ${memory.numbers} CalcArr: ${memory.calcArr}| OpType: ${memory.operationType}`)
+      console.log(result);
       break;
     case 'subtraction':
       break;
